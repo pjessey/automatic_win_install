44c61fa517fa56e6e613761ed51392699c2209f4
pjessey <39692474+pjessey@users.noreply.github.com>
2021-05-02 00:07:17 +0200

    Update launch_install.cmd

--- launch_install.cmd
+++ launch_install.cmd
@@ -1,4 +1,4 @@
 
 
-set url=http://boxstarter.org/package/url?https://raw.githubusercontent.com/pjessey/automatic_win_install/master/bootstrapwin10.ps1
+set url=http://boxstarter.org/package/url?https://raw.githubusercontent.com/pjessey/automatic_win_install/main/bootstrapwin10.ps1
 START %url%
